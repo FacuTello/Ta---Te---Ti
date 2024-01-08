67cc79cf8e8a4866a4ddb7de95f7b5ef5a99c686
FacuTello <facundostello@hotmail.com>
2024-01-08 17:17:07 -0300

    js code is done

--- Index.js
+++ Index.js
@@ -1,4 +1,8 @@
+"use strict";
 
+let gameboardCheck = [];
+let isWinner = false;
+let countTurns = 0;
 
 let playerName1 = prompt("¿Como es tu nombre?");
 let playerName2 = prompt("¿Contra quien vas a jugar?");
@@ -18,7 +22,6 @@ for (let i = 0; i < 9; i++) {
 
 const spots = document.querySelectorAll('.spot');
 
-
 const player1 = {
     name : playerName1,
     mark : "X",
@@ -31,7 +34,6 @@ const player2 = {
     turn : false
 }
 
-
 const changeTurn = ()=>{
     if(player1.turn){
         player1.turn = false;
@@ -42,39 +44,71 @@ const changeTurn = ()=>{
     }
 }
 
-let gameboardCheck = [];
 
-const checkWinner = (gameboardCheck)=>{
- for (let i = 0; i < gameboardCheck.length; i++) {
-    if (gameboardCheck[i] == "X" && gameboardCheck[i+1] == "X" && gameboardCheck[i+2] == "X" ||
-        gameboardCheck[i] == "X" && gameboardCheck[i+3] == "X" && gameboardCheck[i+6] == "X" ||
-        gameboardCheck[i] == "X" && gameboardCheck[i+4] == "X" && gameboardCheck[i+8] == "X" ||
-        gameboardCheck[i+2] == "X" && gameboardCheck[i+4] == "X" && gameboardCheck[i+6] == "X"){
+const checkWinnerRows = (gameboardCheck)=>{
+    if (gameboardCheck[0] == "X" && gameboardCheck[1] == "X" && gameboardCheck[2] == "X" ||
+        gameboardCheck[3] == "X" && gameboardCheck[4] == "X" && gameboardCheck[5] == "X" ||
+        gameboardCheck[6] == "X" && gameboardCheck[7] == "X" && gameboardCheck[8] == "X"   ){
                 console.log("gano el jugador 1");
-        } else if (gameboardCheck[i] == "O" && gameboardCheck[i+1] == "O" && gameboardCheck[i+2] == "O" ||
-        gameboardCheck[i] == "O" && gameboardCheck[i+3] == "O" && gameboardCheck[i+6] == "O" ||
-        gameboardCheck[i] == "O" && gameboardCheck[i+4] == "O" && gameboardCheck[i+8] == "O" ||
-        gameboardCheck[i+2] == "O" && gameboardCheck[i+4] == "O" && gameboardCheck[i+6] == "O"){
+                isWinner = true;
+        } else if (gameboardCheck[0] == "O" && gameboardCheck[1] == "O" && gameboardCheck[2] == "O" ||
+        gameboardCheck[3] == "O" && gameboardCheck[4] == "O" && gameboardCheck[5] == "O" ||
+        gameboardCheck[6] == "O" && gameboardCheck[7] == "O" && gameboardCheck[8] == "O"){
                 console.log("gano el jugador 2");
-        }
-    } 
+                isWinner = true;
+        } 
 }
-        
-const game = ()=>{
+
+const checkWinnerColumns = (gameboardCheck)=>{
+    if (gameboardCheck[0] == "X" && gameboardCheck[3] == "X" && gameboardCheck[6] == "X" ||
+        gameboardCheck[1] == "X" && gameboardCheck[4] == "X" && gameboardCheck[7] == "X" ||
+        gameboardCheck[2] == "X" && gameboardCheck[5] == "X" && gameboardCheck[8] == "X"   ){
+                console.log("gano el jugador 1");
+                isWinner = true;
+        } else if (gameboardCheck[0] == "O" && gameboardCheck[3] == "O" && gameboardCheck[6] == "O" ||
+        gameboardCheck[1] == "O" && gameboardCheck[4] == "O" && gameboardCheck[7] == "O" ||
+        gameboardCheck[2] == "O" && gameboardCheck[5] == "O" && gameboardCheck[8] == "O"){
+                console.log("gano el jugador 2");
+                isWinner = true;
+        } 
+}
+
+const checkWinnerDiagonals = (gameboardCheck)=>{
+    if (gameboardCheck[0] == "X" && gameboardCheck[4] == "X" && gameboardCheck[8] == "X" ||
+    gameboardCheck[2] == "X" && gameboardCheck[4] == "X" && gameboardCheck[6] == "X" ){
+        console.log("gano el jugador 1");
+                isWinner = true;
+    } else if (gameboardCheck[0] == "O" && gameboardCheck[4] == "O" && gameboardCheck[8] == "O" ||
+    gameboardCheck[2] == "O" && gameboardCheck[4] == "O" && gameboardCheck[6] == "O"){
+        console.log("gano el jugador 2");
+        isWinner = true;
+    }
+}
+
+const isDraw = (countTurns, isWinner)=>{
+ if (countTurns == 9 && !isWinner){
+    console.log("es un empate");
+ }
+}
+
+const game = (countTurns)=>{
     for (let i = 0; i < 9; i++) {
-        spots[i].addEventListener('mousedown', ()=> {
+        spots[i].addEventListener('click', ()=> {
             if(player1.turn){
                 spots[i].textContent = player1.mark;
                 gameboardCheck[i] = player1.mark;
+                countTurns += 1;
             } else{
                 spots[i].textContent = player2.mark;
                 gameboardCheck[i] = player2.mark;
+                countTurns += 1;
             }
             changeTurn();
-            checkWinner(gameboardCheck);
-           
+            checkWinnerRows(gameboardCheck);
+            checkWinnerColumns(gameboardCheck);
+            checkWinnerDiagonals(gameboardCheck);
+            isDraw(countTurns, isWinner);
         });
     }
 };
-
 game();
